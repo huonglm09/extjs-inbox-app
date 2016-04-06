f7660e5ab9d042c99778610d9e2e803c79f93caf
unknown <huonglm@qs152-PC.qsoft.com.vn>
2016-04-06 15:41:32 +0700

    update

--- build/temp/development/InboxManagement/sass/config.rb
+++ build/temp/development/InboxManagement/sass/config.rb
@@ -1,2 +1,2 @@
 Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
-cache_path = 'E:\Github\InboxManagement/build/.sass-cache'
+cache_path = 'C:\xampp\htdocs\InboxManagement/build/.sass-cache'

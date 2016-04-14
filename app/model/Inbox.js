cc55db5d18e908076ba7bc90145a4da9752389c9
phan ha <hapt@qsoft.com.vn>
2016-04-14 14:46:34 +0700

    fix sent

--- app/model/Inbox.js
+++ app/model/Inbox.js
@@ -1,44 +1,42 @@
 Ext.define('InboxManagement.model.Inbox', {
-	extend: 'InboxManagement.model.Base',
-
-	fields: [{
-		name: 'id',
-		type: 'integer'
-	}, {
-		name: 'from_user_email',
-		type: 'string',
-		reference: 'User'
-	}, {
-		name: 'to_user_email',
-		type: 'string',
-		reference: 'User'
-	}, {
-		name: 'mail_subject',
-		type: 'string'
-	}, {
-		name: 'from_user',
-		type: 'auto',
-		convert: function(value) {
-			return value.firstName + ' ' + value.lastName;
-		}
-	}, {
-		name: 'created_at',
-		type: 'date',
-		convert: function(value) {
-			return window.moment(value).format('dddd MMM DD, YYYY, h:mm:ss a');
-			/*return new Date(value).toDateString();*/
-		}
-	}],
-
-	proxy: {
-		type: 'rest',
-		url: InboxManagement.Global.getApiUrl() + 'email-inbox/' + localStorage.getItem('email'),
-		headers: {
-			'Accept': 'application/json',
-			'Authorization': 'Bearer ' + localStorage.getItem('access_token')
-		},
-		reader: {
-			rootProperty: 'emails'
-		}
-	}
+    extend: 'InboxManagement.model.Base',
+    fields: [{
+            name: 'id',
+            type: 'integer'
+        }, {
+            name: 'from_user_email',
+            type: 'string',
+            reference: 'User'
+        }, {
+            name: 'to_user_email',
+            type: 'string',
+            reference: 'User'
+        }, {
+            name: 'mail_subject',
+            type: 'string'
+        }, {
+            name: 'from_user',
+            type: 'auto',
+            convert: function (value) {
+                return value.firstName + ' ' + value.lastName;
+            }
+        }, {
+            name: 'created_at',
+            type: 'date',
+            convert: function (value) {
+                return window.moment(value).format('dddd MMM DD, YYYY, h:mm:ss a');
+                /*return new Date(value).toDateString();*/
+            }
+        }],
+    proxy: {
+        type: 'rest',
+        url: InboxManagement.Global.getApiUrl() + 'email-inbox/' + localStorage.getItem('email'),
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem('access_token')
+        },
+        reader: {
+            rootProperty: 'emails'
+        }
+    }
 });
--- app/model/Sent.js
+++ app/model/Sent.js
@@ -9,14 +9,19 @@ Ext.define('InboxManagement.model.Sent', {
             reference: 'User'
         }, {
             name: 'to_user_email',
-            type: 'string',
-            reference: 'User'
+            type: 'auto',
+            convert: function (value) {
+                return value;
+            }
         }, {
             name: 'mail_subject',
             type: 'string'
         }, {
             name: 'created_at',
-            type: 'date'
+            type: 'date',
+            convert: function (value) {
+                return window.moment(value).format('dddd MMM DD, YYYY, h:mm:ss a');
+            }
         }],
     proxy: {
         type: 'rest',
